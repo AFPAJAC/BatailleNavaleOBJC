004f68c9efe4ece0c174d2d7e11ec2d98216fd1f
ios06 <afpaios06@gmail.com>
2015-08-06 17:37:20 +0200

    - StoryBoard avec liaisons dans le ViewController

--- BatailleNavaleOBJC/ViewController.h
+++ BatailleNavaleOBJC/ViewController.h
@@ -9,6 +9,12 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *affichageCoups;
+@property (weak, nonatomic) IBOutlet UILabel *affichageScore;
+
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *boutonsGrille;
+
+- (IBAction)touchDownGrille:(UIButton *)sender;
 
 
 @end
